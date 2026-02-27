265291c330d9c69cf47a44cb0ca1ca5bb7b71494
junhee17 <junheelim9@gmail.com>
2026-02-27 16:58:51 +0900

    변수 설명에 대한 주석 추가

--- src/ch02_VarAndType/Variable2.java
+++ src/ch02_VarAndType/Variable2.java
@@ -2,7 +2,7 @@ package ch02_VarAndType;
 
 //변수 초기화 하기
 public class Variable2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      // main() 자바 프로그램의 첫시작
         int level = 10;      // level  변수 선언과 동시에 값을 대입 (변수 초기화)
         System.out.println(level);
     }
@@ -13,4 +13,7 @@ public class Variable2 {
 변수의 초기화 : 변수에 처음 값을 대입하는 것
 변수의 초기화는 반드시 선언과 동시에 해야하는 것은 아니며,
 변수를 선언한 이후 대입할 값이 정해지는 시점에 초기화 할 수도 있음
+
+변수의 이름은 영문자(대문자, 소문자), 숫자 사용가능
+숫자로 시작 불가능, 자바의 예약어 사용 불가능
  */
