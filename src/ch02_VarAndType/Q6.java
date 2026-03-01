01e31040258919ca5312cf5ccfaaf6d002a4bab2
junhee17 <junheelim9@gmail.com>
2026-03-01 17:43:11 +0900

    6

---
+++ src/ch02_VarAndType/Q6.java
@@ -0,0 +1,10 @@
+package ch02_VarAndType;
+
+// 변수 선언하여 사용하기
+public class Q6 {
+    public static void main (String[] args) {
+        int age;
+        age = 22;
+        System.out.println(age);
+    }
+}
