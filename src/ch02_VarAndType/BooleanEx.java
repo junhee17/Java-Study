5c9620ab38952c1615e9d8bb39cfb5627a4d5409
junhee17 <junheelim9@gmail.com>
2026-03-01 15:33:10 +0900

    논리형 변수를 선언하고 값을 출력하기

---
+++ src/ch02_VarAndType/BooleanEx.java
@@ -0,0 +1,10 @@
+package ch02_VarAndType;
+
+// 논리형 변수를 선언하고 값을 출력하기
+// 결혼여부를 묻는 코드
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean isMarried = true;      // boolean 변수명;
+        System.out.println(isMarried);
+    }
+}
